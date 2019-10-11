4efc5b2bcb9c805128920bc56ec88189bce49c27
Smurf <sudaru2@gmail.com>
2019-10-11 17:18:57 +0300

    fix countTruesaInLine for BoolMatrix

--- boolMatrix.go
+++ boolMatrix.go
@@ -114,6 +114,7 @@ func (m *BoolMatrix) CountTruesInLine(line int) int {
 	res := 0
 	for i := 0; i < m.width; i++ {
 		res += int(m.matrix[lineStart+counter/8] & byte(math.Pow(2, float64(counter%8))))
+		counter++
 	}
 	return res
 }
