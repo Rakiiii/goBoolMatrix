20ef5727129c5028874536da35f509dc9aaa9be0
Smurf <sudaru2@gmail.com>
2019-10-11 14:40:22 +0300

    add bool matrix struct ; add boolMatrix init funcion

--- boolMatrix.go
+++ boolMatrix.go
@@ -1,1 +1,15 @@
 package boolmatrixlib
+
+import "math"
+
+//BoolMatrix is struct that simulate matrix of bools in linair massive with per bit value storage
+type BoolMatrix struct {
+	matrix       []byte
+	width, heigh int
+}
+
+func (m *BoolMatrix) Init(w, h int) {
+	m.matrix = make([]byte, int64(math.Ceil((float64(w)*float64(h))/8)))
+	m.width = w
+	m.heigh = h
+}
